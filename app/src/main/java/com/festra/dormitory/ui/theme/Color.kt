664e7a62ed19127eb87b04be551be52dfc601f7d
faisalabdulr <programeralisal22@gmail.com>
2024-05-05 23:17:06 +0700

    Login Festra coba

--- app/src/main/java/com/festra/dormitory/ui/theme/Color.kt
+++ app/src/main/java/com/festra/dormitory/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.festra.dormitory.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
+val Purple80 = Color(0xFF4D05F7)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
+val PurpleGrey40 = Color(0xFF5103F7)
 val Pink40 = Color(0xFF7D5260)
+val GreenJC = Color(0xFF1D37E4)
