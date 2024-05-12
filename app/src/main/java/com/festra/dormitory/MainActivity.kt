8b3c58229462733f67f2522ce131ca020e81d00d
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2024-05-13 04:26:57 +0700

    Update merge

--- app/src/main/java/com/festra/dormitory/MainActivity.kt
+++ app/src/main/java/com/festra/dormitory/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -12,7 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.festra.dormitory.ui.theme.DormitoryAppTheme
-import com.festra.dormitory.ui.theme.Screen.Login
+import com.festra.dormitory.ui.screen.Login
 
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/festra/dormitory/ui/screen/Login.kt
+++ app/src/main/java/com/festra/dormitory/ui/screen/Login.kt
@@ -1,6 +1,5 @@
-package com.festra.dormitory.ui.theme.Screen
+package com.festra.dormitory.ui.screen
 
-import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
@@ -36,10 +34,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.festra.dormitory.R
-import com.festra.dormitory.ui.theme.DormitoryAppTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
