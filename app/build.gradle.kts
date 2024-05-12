3888b698793fc35afee25d0ce27d764d37b79c79
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2024-05-13 04:13:52 +0700

    Home Screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,12 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //    material3
+    implementation("androidx.compose.material3:material3:1.2.1")
+
+//    navigation
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    // foundation
+    implementation("androidx.compose.foundation:foundation:1.6.7")
 }
