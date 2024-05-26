b1778a02327a44132cccb95757c536872a922d9b
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2024-05-26 20:30:57 +0700

    Merge fitur admin, error drawable belum masuk pt 3

--- app/src/main/java/com/festra/dormitory/ui/screen/AirminumScreen.kt
+++ app/src/main/java/com/festra/dormitory/ui/screen/AirminumScreen.kt
@@ -1,85 +1,83 @@
 package com.festra.dormitory.ui.screen
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.ShoppingCart
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.festra.dormitory.R
+import com.festra.dormitory.navigation.Screen
 import com.festra.dormitory.ui.theme.DormitoryAppTheme
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
-fun AturanScreen(navController: NavController) {
+fun AirminumScreen(navController: NavController) {
     // data store
 //    val dataStore = SettingsDataStore(LocalContext.current)
 //    val showList by dataStore.layoutFlow.collectAsState(true)
 
     val items = listOf(
         BottomNavigationItem(
-            title = "History",
+            title = "history",
             selectedIcon = Icons.Filled.ShoppingCart,
             unselectedIcon = Icons.Outlined.ShoppingCart
         ),
         BottomNavigationItem(
-            title = "Home",
+            title = "home",
             selectedIcon = Icons.Filled.Home,
             unselectedIcon = Icons.Outlined.Home
         ),
         BottomNavigationItem(
-            title = "Profile",
+            title = "profile",
             selectedIcon = Icons.Filled.AccountCircle,
             unselectedIcon = Icons.Outlined.AccountCircle
         ),
     )
 
     var selectedIconIndex by rememberSaveable {
-        mutableIntStateOf(1)
+        mutableIntStateOf(2)
     }
 
     Scaffold(
         topBar = {
-            TopAppBar(
-                navigationIcon = {
-                    IconButton(onClick = { navController.popBackStack() }) {
-                        Icon(
-                            imageVector = Icons.Filled.ArrowBack,
-                            contentDescription = "Kembali",
-                            tint = MaterialTheme.colorScheme.primary
-                        )
-                    }
-                },
-                title = { Text(text = "Aturan") },
+            CenterAlignedTopAppBar(
+                title = { Text(text = "Air Minum") },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.surfaceBright,
                     titleContentColor = MaterialTheme.colorScheme.primary
@@ -114,7 +112,7 @@ fun AturanScreen(navController: NavController) {
                         selected = selectedIconIndex == index,
                         onClick = {
                             selectedIconIndex = index
-//                            navController.navigate(item.title)
+                            navController.navigate(item.title)
                         },
                         label = {
                             Text(text = item.title)
@@ -133,51 +131,79 @@ fun AturanScreen(navController: NavController) {
             }
         }
     ) { paddingValues ->
-        AturanContent(modifier = Modifier.padding(paddingValues))
+        AirminumItem(modifier = Modifier.padding(paddingValues), navController)
     }
 }
 
 @Composable
-fun AturanContent(
-    modifier: Modifier
-) {
+fun AirminumItem(modifier: Modifier = Modifier, navController: NavController) {
+    var namaLengkap by remember { mutableStateOf(TextFieldValue("")) } 
+    var nim by remember { mutableStateOf(TextFieldValue("")) }
+    var nomorGedungKamar by remember { mutableStateOf(TextFieldValue("")) }
+    var jumlahGalon by remember { mutableStateOf(TextFieldValue("")) }
+    var jenisPembayaran by remember { mutableStateOf(TextFieldValue("")) }
+
     Column(
         modifier = modifier
-            .fillMaxWidth()
-            .verticalScroll(rememberScrollState()),
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Simplify your\n" +
-                    "dorm management \n" +
-                    "with Festra"
+        // Input fields
+        OutlinedTextField(
+            value = namaLengkap,
+            onValueChange = { namaLengkap = it },
+            label = { Text("Nama Lengkap") }
         )
-
-        Image(
-            painter = painterResource(id = R.drawable.logo5050_1),
-            contentDescription = "Festra"
+        Spacer(modifier = Modifier.height(8.dp))
+        OutlinedTextField(
+            value = nim,
+            onValueChange = { nim = it },
+            label = { Text("NIM") }
         )
-
-        Text(
-            text = ".\n" +
-                    "\n" +
-                    "Festra dapat menyediakan layanan berupa platform atau website yang memudahkan para penghuni asrama untuk mengakses informasi seputar asrama, termasuk mengelola fasilitas asrama dan berkomunikasi dengan pengelola asrama.\n" +
-                    "\n" +
-                    "Platform yang disediakan oleh Festra dapat berisi informasi terkait jadwal kegiatan asrama, informasi terkait fasilitas asrama, serta informasi penting lainnya seperti pengumuman atau aturan-aturan yang berlaku di dalam asrama. Dengan platform ini, para penghuni asrama dapat dengan mudah mengakses informasi tersebut kapan saja dan di mana saja tanpa perlu bertemu langsung dengan pengelola asrama.\n" +
-                    "\n" +
-                    "Selain itu, platform yang disediakan oleh Festra juga dapat dilengkapi dengan fitur-fitur lain yang dapat memudahkan para penghuni asrama dalam mengelola kebutuhan mereka di dalam asrama. Misalnya, fitur reservasi atau pemesanan fasilitas asrama seperti ruangan rapat, lapangan olahraga, atau fasilitas lainnya yang dapat digunakan oleh para penghuni asrama. Dengan adanya fitur tersebut, penghuni asrama dapat dengan mudah mengelola dan menggunakan fasilitas asrama tanpa perlu menghubungi pengelola asrama secara langsung.\n" +
-                    "\n" +
-                    "Selain itu, platform yang disediakan oleh Festra juga dapat dilengkapi dengan fitur komunikasi antara para penghuni asrama dan pengelola asrama. Fitur ini memungkinkan para penghuni asrama untuk mengajukan pertanyaan, keluhan, atau permintaan bantuan kepada pengelola asrama secara online. Hal ini dapat memudahkan penghuni asrama dalam berkomunikasi dengan pengelola asrama, terutama jika mereka tidak bisa bertemu langsung atau ketika pengelola asrama sibuk.\n" +
-                    "\n" +
-                    "Dengan menyediakan platform atau website yang memudahkan para penghuni asrama dalam mengakses informasi dan mengelola fasilitas asrama, Festra dapat membantu penghuni asrama untuk lebih efektif dan efisien dalam mengelola kebutuhan mereka di dalam asrama. Sehingga, Festra memiliki potensi untuk menjadi salah satu penyedia layanan asrama yang terpercaya dan terdepan.",
-            textAlign = TextAlign.Justify
+        Spacer(modifier = Modifier.height(8.dp))
+        OutlinedTextField(
+            value = nomorGedungKamar,
+            onValueChange = { nomorGedungKamar = it },
+            label = { Text("Nomor Gedung & Kamar") }
         )
+        Spacer(modifier = Modifier.height(8.dp))
+        OutlinedTextField(
+            value = jumlahGalon,
+            onValueChange = { jumlahGalon = it },
+            label = { Text("Jumlah Galon") }
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        OutlinedTextField(
+            value = jenisPembayaran,
+            onValueChange = { jumlahGalon = it },
+            label = { Text("Jenis Pembayaran") }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        // Submit and Cancel buttons
+        Row {
+            Button(
+                onClick = { /* Handle submit */ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Green)
+            ) {
+                Text("Submit")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(
+                onClick = { navController.navigate(Screen.Home.route) },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
+            ) {
+                Text("Cancel")
+            }
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun AturanPreview() {
+fun AirminumPreview() {
     DormitoryAppTheme {
-        AturanScreen(rememberNavController())
+        AirminumScreen(rememberNavController())
     }
 }
